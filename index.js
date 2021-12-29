2c966256fc55805b71f191284991103b2ab2fc42
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2021-12-28 21:24:31 -0300

    Manage to get fetch all pairs

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 // *************************************************************
 // *                        REQUIRES
 // *************************************************************
-const {ChainId, Fetcher, WETH, Route} = require("@uniswap/sdk");
+const {ChainId, Fetcher, Route} = require("@uniswap/sdk");
 const {ethers} = require("ethers");
-const fs = require("fs");
+const {Address} = require("cluster");
+
+const tokensJson = require("./tokens.json");
+const keys = require("./keys.json");
 
 
 // *************************************************************
 // *                      CONSTANTS
 // *************************************************************
 const chainId = ChainId.MAINNET;
-const usdcAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 
 
 // *************************************************************
 // *                        CODE
 // *************************************************************
-let rawdata = fs.readFileSync('tokens.json');
-let pairsJson = JSON.parse(rawdata);
-console.log(pairsJson);
+const init = async (tokensJson) => {
+    let tokens = {};
+    let routes = [];
+    const provider = new ethers.providers.JsonRpcProvider(keys.httpsInfuraEndpoint);
 
-const init = async () => {
-    const usdc = await Fetcher.fetchTokenData(chainId, usdcAddress);
-    const dai = await Fetcher.fetchTokenData(chainId, daiAddress);
+    for (const [token, address] of Object.entries(tokensJson)) {
+        tokens[token] = await Fetcher.fetchTokenData(chainId, address, provider);
+    }
 
-    const usdc2daiPair = await Fetcher.fetchPairData(usdc, dai);
-    const dai2wethPair = await Fetcher.fetchPairData(dai, WETH[chainId]);
+    // After I am sure that all tokens have been fetched...
+    for (const token in tokens) {
+        // Adding route from USDC to token.
+        if (token != "USDC") {ß
+            const pair = await Fetcher.fetchPairData(tokens["USDC"], tokens[token], provider);
+            const route = new Route([pair], tokens["USDC"]);
+            routes.push({
+                "route": route,
+                "tokens": ["USDC", token]
+            });
 
-    const usdc2daiRoute = new Route([usdc2daiPair], usdc);
-    const dai2wethRoute = new Route([dai2wethPair], dai);
+            // Adding route from WETH to token.
+            if (token != "WETH") {
+                const pair = await Fetcher.fetchPairData(tokens["WETH"], tokens[token], provider);
+                const route = new Route([pair], tokens["WETH"]);
+                routes.push({
+                    "route": route,
+                    "tokens": ["WETH", token]
+                });
+            }
+        }
+    }
 
-    console.log(usdc2daiRoute.midPrice.toSignificant(6));
-    console.log(dai2wethRoute.midPrice.toSignificant(6));
+    return routes;
 }
 
-init();
+init(tokensJson).then(response => {
+    for (const route in response) {
+        console.log(response[route]["tokens"], response[route]["route"].midPrice.toSignificant(6));
+    }
+});
